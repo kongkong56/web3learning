7af3dd0c1208318e19009320c1afd5b8b28e562a
kongkong56 <kongkong56@gmail.com>
2022-06-24 12:24:27 +0800

    feat: init

---
+++ README1'.md
@@ -0,0 +1,15 @@
+# web3learning
+
+### 1.初始化项目
+npx create-next-app web3-blog
+
+cd web3-blog
+
+### 2.安装依赖包
+npm install ethers hardhat @nomiclabs/hardhat-waffle 
+npm install ethereum-waffle chai @nomiclabs/hardhat-ethers
+npm install web3modal @walletconnect/web3-provider 
+npm install easymde react-markdown react-simplemde-editor
+npm install ipfs-http-client @emotion/css @openzeppelin/contracts
+###  3.
+npx hardhat创建一个项目
---
+++ web3-blog/scripts/deploy.js
@@ -0,0 +1,58 @@
+// // We require the Hardhat Runtime Environment explicitly here. This is optional
+// // but useful for running the script in a standalone fashion through `node <script>`.
+// //
+// // When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// // Runtime Environment's members available in the global scope.
+// const hre = require("hardhat");
+
+// async function main() {
+//   // Hardhat always runs the compile task when running scripts with its command
+//   // line interface.
+//   //
+//   // If this script is run directly using `node` you may want to call compile
+//   // manually to make sure everything is compiled
+//   // await hre.run('compile');
+
+//   // We get the contract to deploy
+//   const Greeter = await hre.ethers.getContractFactory("Greeter");
+//   const greeter = await Greeter.deploy("Hello, Hardhat!");
+
+//   await greeter.deployed();
+
+//   console.log("Greeter deployed to:", greeter.address);
+// }
+
+// // We recommend this pattern to be able to use async/await everywhere
+// // and properly handle errors.
+// main()
+//   .then(() => process.exit(0))
+//   .catch((error) => {
+//     console.error(error);
+//     process.exit(1);
+//   });
+/* scripts/deploy.js */
+const hre = require("hardhat");
+const fs = require('fs');
+
+async function main() {
+  /* these two lines deploy the contract to the network */
+  const Blog = await hre.ethers.getContractFactory("Blog");
+  const blog = await Blog.deploy("My blog");
+
+  await blog.deployed();
+  console.log("Blog deployed to:", blog.address);
+
+  /* this code writes the contract addresses to a local */
+  /* file named config.js that we can use in the app */
+  fs.writeFileSync('./config.js', `
+  export const contractAddress = "${blog.address}"
+  export const ownerAddress = "${blog.signer.address}"
+  `)
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
