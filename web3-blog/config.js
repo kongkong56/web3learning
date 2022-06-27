0dac406b950a2bbf6f94bfce62ac9cc93878904e
kongkong56 <kongkong56@gmail.com>
2022-06-27 11:17:18 +0800

    feat: 页面代码

--- web3-blog/config.js
+++ web3-blog/config.js
@@ -1,4 +1,2 @@
-
-  export const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-  export const ownerAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
-  
+export const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+export const ownerAddress = "0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199"
